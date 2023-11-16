e718e51b2c3b00c6e9083ac42a87507022f3b2da
bbearcookie <aodem@naver.com>
2023-11-16 21:59:45 +0900

    refactor: FCM SW 정리

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,24 @@
 self.addEventListener('install', function (e) {
-  console.log('fcm sw install..');
+  console.log('[FCM] Service Worker Installed.');
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function (e) {
-  console.log('fcm sw activate..');
+  console.log('[FCM] Service Worker Activated.');
 });
 
 self.addEventListener('push', function (e) {
   const data = e.data.json();
   if (!data) return;
 
-  const resultData = data.notification;
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.body,
-    icon: resultData.image,
-    tag: resultData.tag,
-    ...resultData,
+  console.log('[FCM] Push Received.');
+
+  const title = data.notification.title;
+
+  const options = {
+    icon: data.notification.image,
+    ...data.notification,
   };
-  console.log('push: ', { resultData, notificationTitle, notificationOptions });
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener('notificationclick', function (event) {
-  console.log('notification click');
-  const url = '/';
-  event.notification.close();
-  event.waitUntil(clients.openWindow(url));
+  self.registration.showNotification(title, options);
 });
