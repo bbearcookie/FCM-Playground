e00759b1e8349574f7a2877a01fa9221b00e49ea
bbearcookie <aodem@naver.com>
2023-11-19 23:14:09 +0900

    feat: 권한 변경 감지 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
-import { getMessaging, getToken } from 'firebase/messaging';
-import firebase from './utils/firebase';
+import { getFCMToken } from './services/getFCMToken';
 import './App.css';
 
-function App() {
-  const handlePermission = async () => {
-    const permission = await Notification.requestPermission();
+if ('permissions' in navigator) {
+  navigator.permissions.query({ name: 'notifications' }).then(function (notificationPerm) {
+    notificationPerm.onchange = async function () {
+      console.log('권한 변경 감지:', notificationPerm.state);
 
-    if (permission === 'granted') {
-      alert('Notification permission granted.');
-    }
-  };
+      if (notificationPerm.state === 'granted') {
+        // const token = await getFCMToken();
+        // console.log(token);
+      }
+    };
+  });
+}
+
+function App() {
+  // const handlePermission = async () => {
+  //   const permission = await Notification.requestPermission();
+
+  //   if (permission !== 'granted') {
+  //     alert('브라우저 설정에서 직접 알림 권한을 허용해주세요.');
+  //   }
+  // };
 
   const handleGetToken = async () => {
-    const message = getMessaging(firebase);
-
-    const token = await getToken(message, {
-      vapidKey: 'BLuDu-U9wEKSpnHSmnRvoIClAUx7z3Xf_sI6TaJqh-9JSPtJ10DR8WQsFTp4dEngWqLviJcJZvXK_vu-SkI2ppg',
-    });
-
-    console.log(token);
+    try {
+      const token = await getFCMToken();
+      console.log(token);
+    } catch (err) {
+      alert('브라우저 설정에서 직접 알림 권한을 허용해주세요.');
+    }
   };
 
   return (
     <>
-      <button onClick={handlePermission}>Permission 요청</button>
+      {/* <button onClick={handlePermission}>Permission 요청</button> */}
       <button onClick={handleGetToken}>토큰 GET</button>
     </>
   );
